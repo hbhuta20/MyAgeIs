a9807c7898d08d239378f9ed5137a881c30daf06
Hetansh Bhuta <hetansh.bhuta@patelhs.org>
2020-09-24 19:33:01 +0000

    Today I learned If/Else statemnts and also coded a my birthday, my age and todays date. It was pretty fun to do this assignment.

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
-class Main {
+class CheckMyAge {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int myBirthday = 23;
+    int todaysDate = 24;
+
+    System.out.println(" I am "+ myAge + " years old.");
+    System.out.println(" I was born on "+ myBirthday + "rd Febraury 2005.");
+    System.out.println(" Today is "+ todaysDate +"th September 2020 and it is a Thursday.");
+
+    if(myAge==todaysDate){
+      myAge=myAge+1;
+      System.out.println(" I am "+ myAge +" years old.");
+    }else if(myAge!=todaysDate){
+      System.out.println(" I am " + myAge + " years old.");
+    }
+
   }
 }
